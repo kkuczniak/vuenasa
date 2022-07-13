02a63b52c9f097af28e7af3a3d467c46ad067e3a
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-07-13 21:32:38 +0200

    clean app and added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header class="flex justify-center items-center bg-[#171738] h-12">
+    <router-link to="/">
+      <div class="flex">
+        <h1
+          class="text-[#FFD9CE] tracking-widest text-2xl font-semibold after:content-['images'] after:text-xs after:italic after:tracking-tight"
+        >
+          NASA
+        </h1>
+      </div>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
