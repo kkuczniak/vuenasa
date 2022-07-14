70cfb5f0b84afed2ce9e621b174a76eccf73fcd1
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-07-14 22:56:40 +0200

    added form and api fetching to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,54 @@
 <template>
-  <div class="home"></div>
+  <div class="home mt-5">
+    <div class="imageCard">
+      <router-link to="">hello</router-link>
+    </div>
+    <form @submit.prevent="SearchImages()" class="flex flex-col items-center">
+      <input
+        class="border border-black rounded w-1/2 md:w-44 outline-none"
+        type="text"
+        placeholder="e.g. Saturn"
+        v-model="search"
+      />
+      <input
+        type="submit"
+        value="Search"
+        class="border border-black rounded mt-2 text-center uppercase"
+      />
+    </form>
+    <CardComponent :nasaImages="nasaImages" />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-export default {};
+import { ref } from 'vue';
+import CardComponent from '../components/CardComponent.vue';
+
+export default {
+  name: 'HomeView',
+  components: { CardComponent },
+  setup() {
+    const search = ref('');
+    const nasaImages = ref([]);
+
+    const SearchImages = () => {
+      if (search.value != '') {
+        fetch(`https://images-api.nasa.gov/search?q=${search.value}`)
+          .then((response) => response.json())
+          .then((data) => {
+            nasaImages.value = data.collection.items.slice(0, 9);
+            search.value = '';
+            console.log(nasaImages.value);
+          });
+      }
+    };
+    return {
+      search,
+      nasaImages,
+      SearchImages,
+    };
+  },
+};
 </script>
