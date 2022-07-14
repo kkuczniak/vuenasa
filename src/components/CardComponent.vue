7345c8674741a2e9a0e85031681be76926757e7c
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-07-14 22:56:50 +0200

    added card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="homeComponent w-full h-48 bg-gray-600">
+    <div v-for="nasaImage in nasaImages" :key="nasaImage.id">
+      <router-link :to="'/nasa/' + nasaImage.data[0].nasa_id">
+        <h3 class="text-2xl text-white">{{ nasaImage.data[0].title }}</h3>
+        <p>{{ nasaImage.data[0].description }}</p>
+        <img :src="nasaImage.links[0].href" alt="{{index}}" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['nasaImages'],
+  setup(props) {
+    console.log(props.nasaImages);
+  },
+};
+</script>
+
+<style></style>
